64b91389601979d7d484b75c30aef09624b691d5
bartonmic <bartonmi@trimet.org>
2024-09-12 15:40:41 -0700

    modeling exercises

--- R/tidy_census.R
+++ R/tidy_census.R
@@ -13,7 +13,21 @@ readRenviron("~/.Renviron")
 
 #### User functions ####
 
-####
+tidy_acs_result <- function(raw_result, include_moe=FALSE) {
+  # takes a tidycensus acs result and returns a wide and tidy table
+  if (isTRUE(include_moe)) {
+    new_df <- raw_result |> pivot_wider(id_cols = GEOID:NAME,
+                                        names_from = variable,
+                                        values_from = estimate:moe)
+  } else {
+    new_df <- raw_result |> pivot_wider(id_cols = GEOID:NAME,
+                                        names_from = variable,
+                                        values_from = estimate)
+  }
+  return(new_df)
+}
+
+####)
 
 # get a searchable census variable table
 v19 <- load_variables(2019, "acs5")
@@ -29,7 +43,7 @@ comm_19_raw <- get_acs(geography = "tract",
                        state = "OR",
                        year = 2019,
                        survey = "acs5",
-                       geometry = FALSE # can retrieve library(of)
+                       geometry = FALSE # can retrieve library(sf)
                                         # spatial geoms pre-joined
                        )
 comm_19_raw
@@ -41,3 +55,55 @@ comm_19 <- comm_19_raw |>
 
 comm_19
 
+# using function
+comm_19 <- tidy_acs_result(comm_19_raw)
+comm_19
+
+
+# get 2022 ACS data ----
+comm_22_raw <- get_acs(geography = "tract",
+                       variable = c(wfh = "B08006_017",
+                                    transit = "B08006_008",
+                                    tot = "B08006_001"),
+                       county = "Multnomah",
+                       state = "OR",
+                       year = 2022,
+                       survey = "acs5",
+                       geometry = FALSE # can retrieve library(sf)
+                                        # spatial geoms pre-joined
+)
+
+comm_22 <- tidy_acs_result(comm_22_raw)
+comm_22
+
+# join the years ----
+comm_19_22 <- comm_19 |> 
+  inner_join(
+            comm_22,
+            by="GEOID",
+            suffix = c("_19", "_22")
+            ) |>
+  select(-starts_with("NAME"))
+comm_19_22
+
+# create some change variables ----
+comm_19_22 <- comm_19_22 |>
+  mutate(wfh_chg = wfh_22 - wfh_19,
+         transit_chg = transit_22 - transit_19)
+summary(comm_19_22 |> select(ends_with("_chg")))
+
+# plot them ----
+p <- comm_19_22 |>
+  ggplot(aes(x = wfh_chg, y = transit_chg))
+
+p + geom_point() + geom_smooth(method = "lm") +
+  labs(x="change in WFH",
+       y= "change in transit",
+       title="ACS 2022 vs 2019 (5-year)") +
+  annotate("text", x=800, y=50,
+           label = paste("r =",
+                         round(cor(comm_19_22$wfh_chg,
+                             comm_19_22$transit_chg), 3
+                         )
+           )
+  )
